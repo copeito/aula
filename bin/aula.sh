d1f221fcf2191ff441f2fdba788cfda3861ac1d0
David Rey <copeito@gmail.com>
2019-08-18 16:29:45 +0200

    Detección de directorio de laravel en bin/aula.sh

--- bin/aula.sh
+++ bin/aula.sh
@@ -1,2 +1,3 @@
-LARAPATH=`/bin/readlink -f ../laravel`
-docker run -v $LARAPATH/:/var/www/html -p 80:80 -p 3306:3306 -it aula
+WWW=$(readlink -f `dirname $0` | sed "s/\/bin$/\/laravel/")
+docker run -v $WWW:/var/www/html -p 80:80 -p 3306:3306 -it aula
+
